b44a6e706c7969b2900da89e8c1dca8fc0538caf
EmyPGB <90361941+EmyPGB@users.noreply.github.com>
2022-11-02 13:55:04 -0400

    can create journal entries

--- journals_and_ledgers/urls.py
+++ journals_and_ledgers/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
 
+    path('journal_entry/', views.render_journal_entry_page),
+    path('manager/unapproved_entries/', views.render_unapproved_entries_page),
+    path('new_journal_entry_request/submit/', views.submit_request_for_new_journal_entry),
 ]
