7db0cf50767aaec6cae776c170f11d753eaa52aa
Mason Cochran <52899575+BrodyBrosius@users.noreply.github.com>
2022-11-04 12:57:47 -0400

    Added functionality to approve journal entry requests via manager. However, rejection is still WIP

--- journals_and_ledgers/models.py
+++ journals_and_ledgers/models.py
@@ -1,22 +1,37 @@
 from enum import unique
 from django.db import models
 from accounts.models import Account
+from users.models import User
 
 # Create your models here.
 
 
+
+
 class JournalEntry(models.Model):
     journal_entry_id = models.IntegerField(unique=True, primary_key=True)
+    user_submission_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    initial_entry_description = models.CharField(max_length=200)
     date_of_entry = models.DateField(auto_now_add=True)
     account_debit_id = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='account_debit_id')
     account_credit_id = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='account_credit_id')
-    debit_amount = models.DecimalField(max_digits=19, decimal_places=10)
-    credit_amount = models.DecimalField(max_digits=19, decimal_places=10)
+    debit_amount = models.DecimalField(max_digits=19, decimal_places=2)
+    credit_amount = models.DecimalField(max_digits=19, decimal_places=2)
     journal_comment = models.TextField(max_length=200)
     is_approved = models.BooleanField(default=False)
+    approver_id = models.CharField(default='Unapproved', max_length=50)
+
+    def approve_journal(self):
+        self.is_approved = True
+
+    def set_approver_id(self, approver_id):
+        self.approver_id = approver_id
 
 
 class TransactionError(models.Model):
     error_id = models.IntegerField(unique=True, primary_key=True)
     error_date_time = models.DateTimeField(auto_now_add=True)
     error_desc = models.CharField(max_length=1000)
+
+
+
--- journals_and_ledgers/urls.py
+++ journals_and_ledgers/urls.py
@@ -7,6 +7,8 @@ from . import views
 urlpatterns = [
 
     path('journal_entry/', views.render_journal_entry_page),
-    path('manager/unapproved_entries/', views.render_unapproved_entries_page),
+    path('manager/unapproved_entries/', views.render_unapproved_entries_page), #Review because of manager bit
     path('new_journal_entry_request/submit/', views.submit_request_for_new_journal_entry),
+    path('new_journal_entry_request/approve/', views.commit_journal_entry_approval),
+    path('new_journal_entry_request/reject/', views.reject_journal_entry),
 ]
